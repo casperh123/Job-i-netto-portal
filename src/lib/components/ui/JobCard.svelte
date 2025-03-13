65bd7b8e9f34da3e604c480f655d9f57ea344b92
casperh123 <casho@itu.dk>
2025-03-13 21:11:52 +0100

    Add job card styling

---
+++ src/lib/components/ui/JobCard.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    let { job } = $props();
+</script>
+
+<div class="job-card">
+    <div class="title-address-wrapper">
+        <h3>{job.title}</h3>
+        <p>{`${job.address.street} - ${job.address.zip}, ${job.address.city}`}</p>
+    </div>
+    <div class="categories-wrapper">
+        {#each job.categories as category}
+            <p class="category">{category}</p>
+        {/each}
+    </div>
+    <p>{job.address.city}</p>
+    <button class="btn primary">Se Mere</button>
+</div>
+
+<style>
+    .job-card {
+        display: grid;
+        grid-template-columns: 3fr repeat(3, 1fr);
+        align-items: center;
+        justify-content: space-between;
+        gap: 10px;
+        padding: 20px 30px;
+        background-color: #242424;
+        border: 1px solid white;
+        border-radius: 8px;
+        color: white;
+    }
+
+    .title-address-wrapper {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .categories-wrapper {
+        display: grid;
+        grid-gap: 20px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { apiClient } from '$lib/services/api';
+	import JobCard from '$lib/components/ui/JobCard.svelte';
 
 	let jobs : Array<any> = [];
 	let page = 1;
@@ -20,16 +21,17 @@
 	}
 </script>
 
-<h1>Job i Nette</h1>
-<h2>Din Jobportal til Netto</h2>
+<div class="header-wrapper">
+	<h1>Job i Netto</h1>
+	<h2>Din Jobportal til Netto</h2>
+</div>
 
 {#if jobs.length > 0}
-	<ul>
+	<div class="job-cards-wrapper">
 		{#each jobs as job}
-			<li>{job.brand}</li>
-			<li>{job.title}</li>
+			<JobCard job={job}></JobCard>
 		{/each}
-	</ul>
+	</div>
 	<p>Page: {page}</p>
 	<button on:click={() => getPage(1)} disabled={page === 1}>First</button>
 	<button on:click={() => getPage(page - 1)} disabled={page === 1}>Previous</button>
@@ -39,3 +41,18 @@
 {:else}
 	<p>Loading jobs...</p>
 {/if}
+
+<style>
+	.header-wrapper {
+		padding: 20px;
+	}
+
+	.job-cards-wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+		padding: 40px;
+		border-radius: 8px;
+		background-color: #242424;
+	}
+</style>
