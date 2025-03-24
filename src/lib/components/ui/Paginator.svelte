ce48d991d5f272d3cc512574db9184708ac3d7d2
casperh123 <casperholten@me.com>
2025-03-24 20:52:44 +0100

    Add input to select page to paginater

--- src/lib/components/ui/Paginator.svelte
+++ src/lib/components/ui/Paginator.svelte
@@ -1,28 +1,50 @@
 <script lang="ts">
     let { totalPages, pageChanged } = $props();
+    
     let page : number = $state(1);
 
-    function changePage(newPage : number) {
-        if (newPage == page) return;
-        if (newPage < 1 || newPage > totalPages) return;
-
-        page = newPage;
-
-        pageChanged(newPage);
+    function changePage(newPage: number) {
+        if (newPage < 1) {
+            newPage = 1;
+        } else if (newPage > totalPages) {
+            newPage = totalPages;
+        }
+        
+        if (newPage !== page) {
+            page = newPage;
+            pageChanged(newPage);
+        }
+    }
+    
+    function handleInputChange(event: Event) {
+        const input = event.target as HTMLInputElement;
+        let newPage = parseInt(input.value);
+        
+        if (isNaN(newPage)) {
+            input.value = page.toString();
+            return;
+        }
+        
+        changePage(newPage);
+        
+        input.value = page.toString();
     }
 </script>
 
 <div class="pagination-wrapper flex row">
     <div class="button-group flex row">
-        <button onclick={() => changePage(1)} disabled={page === 1}>First</button>
-        <button onclick={() => changePage(page - 1)} disabled={page === 1}>Previous</button>
+        <button onclick={() => changePage(1)} disabled={page === 1}>Første</button>
+        <button onclick={() => changePage(page - 1)} disabled={page === 1}>Forrige</button>
     </div>
 
-    <p class="page-counter">{page}</p>
+    <div class="flex gap-1 items-center">
+        <input class="page-counter" type="number" value={page} onchange={handleInputChange} min="1" max={totalPages}/>
+        <p class="counter">/ {totalPages}</p>
+    </div>
 
     <div class="button-group flex row">
-        <button onclick={() => changePage(page + 1)} disabled={page === totalPages}>Next</button>
-        <button onclick={() => changePage(totalPages)} disabled={page === totalPages}>Last</button>
+        <button onclick={() => changePage(page + 1)} disabled={page === totalPages}>Næste</button>
+        <button onclick={() => changePage(totalPages)} disabled={page === totalPages}>Sidste</button>
     </div>
 </div>
 
@@ -47,12 +69,18 @@
         cursor: pointer;
     }
 
+    .counter {
+        color: #fff;
+    }
+
     .page-counter {
-    background-color: white;
-    color: #242424;
-    border-radius: 8px;
-    padding: 10px 20px; 
-}
+        background-color: white;
+        color: #242424;
+        border-radius: 8px;
+        text-align: center;
+        max-width: 60px;
+        padding: 5px 0;
+    }
 
     .button-group {
         gap: 10px;
@@ -69,7 +97,8 @@
 
     @media only screen and (max-width: 720px) {
         .page-counter {
-            padding: 5px 10px;
+            padding: 2px 10px;
+            font-size: 12px;
         }
 
         .pagination-wrapper button {
